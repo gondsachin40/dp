03eb093743d590b432948df3a7be5dd24b4fa1c2
gondsachin40 <162681936+gondsachin40@users.noreply.github.com>
2024-12-30 15:24:26 +0530

    Create 0) Pracitce.cpp

---
+++ 0) Pracitce.cpp
@@ -0,0 +1,29 @@
+Monday, December 30, 2024
+3:18 PM
+
+Buying apples
+https://www.spoj.com/problems/ABA12C/
+
+Alpha code
+https://www.spoj.com/problems/ACODE/
+
+Tri Tiling
+http://poj.org/problem?id=2663
+
+New Year and the Permutation Concatentation
+https://codeforces.com/problemset/problem/1091/D
+
+Multiplicity
+https://codeforces.com/problemset/problem/1061/C
+
+Consecutive Subsequence
+https://codeforces.com/problemset/problem/977/F
+
+Animals
+https://codeforces.com/problemset/problem/35/D
+
+Soldier and Number Game
+https://codeforces.com/problemset/problem/546/D
+
+Robber's watch
+https://codeforces.com/problemset/problem/685/A
