2048ec5c9ff25158f2dfa6376908f57fab0612dd
gondsachin40 <162681936+gondsachin40@users.noreply.github.com>
2025-12-13 12:57:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,3 +46,84 @@ if __name__ == "__main__":
 # python -m vevn vevn
 # vevn\Scripts\Activate
 # python app.py
+
+
+
+
+from flask import Flask , request ,jsonify,json
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token , JWTManager , jwt_required, get_jwt_identity
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "sachin"
+
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+class User(db.Model):
+    name = db.Column(db.String(50) , nullable = False)
+    password = db.Column(db.String(10) , nullable = False)
+    id = db.Column(db.Integer , primary_key = True)
+
+
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+@app.route("/login" , methods = ["POST"])
+def login():
+    data = request.get_json()
+    
+    if not data or "name" not in data or "password" not in data:
+        return jsonify({"error" : "empty field"}) , 400
+    
+    user = User.query.filter_by(name = data["name"]).first()
+
+    if not user:
+        return jsonify({"error" : "user not exist"}) , 400
+    
+    if user.password != data["password"]:
+        return jsonify({"error" : "password did not match"}) , 400
+        
+    token = create_access_token(identity=user.id)
+    print(token)
+
+    
+    return jsonify({
+        "msg" : "login successful"
+        ,"access_token" : token}) , 200
+    
+
+    
+    
+
+
+@app.route("/signup" , methods = ["POST"])
+def signup():
+    data = request.get_json()
+    if not data or "name" not in data or "password" not in data:
+        return jsonify({"error" : "empty field"}) , 400
+    
+    existing_user = User.query.filter_by(name=data["name"]).first()
+
+    if existing_user:
+        return jsonify({"error": "user already exists"}), 409
+
+    # print(data["name"] , data["password"]);
+    newUser = User(name = data["name"] , password = data["password"])
+
+    
+    db.session.add(newUser)
+    db.session.commit()
+    return "hello"
+
+
+if __name__ == "__main__":
+    print("localhost:5000")
+    app.run(debug=True)
